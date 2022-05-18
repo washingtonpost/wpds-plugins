05f2608b1f46415fdd2a64fec0d7ea340968e9e1
BrianAA <brianalfaro@thoughttolife.com>
2022-05-18 19:54:59 -0400

    Update Config.js

--- adaptive-color-palettes/Config.js
+++ adaptive-color-palettes/Config.js
@@ -1,9 +1,10 @@
+/**This config file is used to change the output of the files generated in teh new_theme json & html. For more indepth of the following settings (lightness, contrast,) */
 const Config = {
 	lightness: 0,
 	contrast: 1,
 	saturation: 100,
 	backgroundColorHTML: "#000000",
-	LeonardoBackground: "#FFFFFF", //This is the target background contrast of the existing color palette
+	LeonardoBackground: "#FFFFFF", 
 };
 
 module.exports = Config;
