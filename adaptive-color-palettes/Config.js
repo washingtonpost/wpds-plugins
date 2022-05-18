3c54a21cafcfba4dc8ddecefcdc55ac87e4cce11
BrianAA <brianalfaro@thoughttolife.com>
2022-05-18 11:30:07 -0400

    Added HTML template

---
+++ adaptive-color-palettes/Config.js
@@ -0,0 +1,8 @@
+const Config = {
+	lightness: 0,
+	contrast: 2,
+	saturation: 100,
+	backgroundColorHTML: "#000000",
+};
+
+module.exports = Config;
--- adaptive-color-palettes/theme.js
+++ adaptive-color-palettes/theme.js
@@ -1,111 +1,167 @@
 const {
-  Theme,
-  Color,
-  BackgroundColor,
+	Theme,
+	Color,
+	BackgroundColor,
 } = require("@adobe/leonardo-contrast-colors");
+const CurrentPalette = require("./ColorPalette");
+const Config = require("./Config");
+const fs = require("fs");
+const { config } = require("process");
+let LeonardoPalette = []; //Holds colors for Leonardo
 
-const tsv = `Token	0	20	40	60	80	100	200	300	400	500	600	700
-gray	#000000	#111111	#2a2a2a	#494949	#666666	#737373	#aaaaaa	#d5d5d5	#e9e9e9	#f0f0f0	#f7f7f7	#ffffff
-blue	-	-	#0a3258	#0f4b84	#1366b3	#166dfc	#5784c5	#84a4d7	#acc5e8	#d3e7fa	#e8f1ff	-
-red	-	-	#63100e	#8e1f1b	#be2c25	#ea0017	#d35f4f	#e58a7c	#f2b4ab	#fbdedc	#ffe8e8	-
-pink	-	-	#690b2c	#951940	#c32755	#ff4f83	#d55e77	#e48a9b	#f0b4c0	#f9dee7	#ffebf1	-
-purple	-	-	#5d1254	#881b79	#b623a1	#d138bf	#c95cb6	#da89ca	#eab3df	#f8ddf4	#fbeffa	-
-teal	-	-	#0b3940	#0e545f	#0f7180	#03afca	#4a8e9b	#75acb6	#9fcbd3	#c8ebf0	#ebfcff	-
-orange	-	-	#763500	#a64a00	#d86100	#f3750e	#e6823d	#f1a26a	#f9c296	#fde2c4	#ffefe0	-
-green	-	-	#285115	#38711e	#499327	#36a75c	#6ea951	#91c078	#b2d6a0	#d4edc9	#effaeb	-
-gold	-	-	#6D4508	#9e6105	#c2882f	#e5a036	#d39e4c	#deb57e	#eacca5	#f3e4cd	#faf0e1	-
-yellow	-	-	-	-	-	#ffec44	-	-	-	-	#fffbda	-
-mustard	-	-	#644801	#916800	#c18b00	#e2a31f	#d99e20	#ebb347	#f8cb70	#ffe49d	#fbf1dd	-`;
+//Start of script
+PrepareData();
 
-let ColorPalette = []; //Holds colors for Leonardo
-FormatData();
-
-// Takes TSV
-function FormatData() {
-  const items = tsv.split("\n");
-  const headerRow = items[0].split("\t");
-  const tokenValue = headerRow.splice(1, headerRow.length);
-  const data = items.splice(1, items.length);
-
-  for (let i = 0; i < data.length; i++) {
-    if (data[i]) {
-      let row = data[i].split("\t");
-      let colorName = row[0];
-      let colorValues = row.splice(1, row.length);
-      for (let v = 0; v < colorValues.length; v++) {
-        if (colorValues[v] != "-") {
-          let tokenNumber = tokenValue[v];
-          const _Color = new Color({
-            name: `${colorName + tokenNumber}`,
-            colorKeys: [`${colorValues[v]}`],
-            ratios: [contrast([255, 255, 255], hexToRgbA(colorValues[v]))],
-          });
-          ColorPalette.push(_Color);
-        }
-      }
-    }
-  }
-
-  SetUpTheme();
+/**Prepares Current Palette data to be added into Leonardo Palette */
+function PrepareData() {
+	for (token in CurrentPalette) {
+		if (CurrentPalette[token].hasOwnProperty("value")) {
+			let _value = CurrentPalette[token].value;
+			//if Color token value is a hex value
+			if (CurrentPalette[token].value[0] == "#") {
+				_value = hexToRgbA(_value);
+			}
+			//Generate Leonardo Color palette
+			const _Color = new Color({
+				name: token,
+				colorKeys: [CurrentPalette[token].value],
+				ratios: [contrast([255, 255, 255], _value)],
+			});
+			//Add to Color Palette
+			LeonardoPalette.push(_Color);
+		}
+	}
+	SetUpTheme();
 }
 
+/**Sets up Leonardo color palette if you like to adjust the lightness, contrast, and saturation modify the global variables found int eh Config.js. */
 function SetUpTheme() {
-  if (!ColorPalette.length) return;
-  let context = new BackgroundColor({
-    name: "context",
-    colorKeys: ["#FFFFFF"],
-    ratios: [21],
-  });
+	if (!LeonardoPalette.length) return;
+	let context = new BackgroundColor({
+		name: "context",
+		colorKeys: ["#FFFFFF"],
+		ratios: [21],
+	});
 
-  let theme = new Theme({
-    colors: [...ColorPalette],
-    backgroundColor: context,
-    lightness: 5,
-  });
-  let NewTheme = {};
-  theme.contrastColors.map((c) => {
-    if (c.values) {
-      NewTheme[c.name] = {
-        value: c.values[0].value,
-      };
-    }
-  });
-  console.log(NewTheme);
+	let theme = new Theme({
+		colors: [...LeonardoPalette],
+		backgroundColor: context,
+		lightness: Config.lightness,
+		contrast: Config.contrast,
+		saturation: Config.saturation,
+	});
+	let NewTheme = {};
+	theme.contrastColors.map((c) => {
+		if (c.values) {
+			NewTheme[c.name] = {
+				value: c.values[0].value,
+			};
+		}
+	});
+	CreateFiles(JSON.stringify(NewTheme));
+}
+/**
+ * This function creates new_theme.html and new_theme.json
+ * @param {Stringify JSON data} content
+ */
+
+function CreateFiles(content) {
+	try {
+		fs.writeFile("./new_theme.json", content, (err) => {
+			fs.writeFile(
+				"./new_theme.html",
+				HTMLContent(NewTheme),
+				(err) => {}
+			);
+		});
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 //Converts hex to RGBA
 function hexToRgbA(hex) {
-  var c;
-  if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-    c = hex.substring(1).split("");
-    if (c.length == 3) {
-      c = [c[0], c[0], c[1], c[1], c[2], c[2]];
-    }
-    c = "0x" + c.join("");
-    let r = (c >> 16) & 255;
-    let g = (c >> 8) & 255;
-    let b = c & 255;
+	var c;
+	if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+		c = hex.substring(1).split("");
+		if (c.length == 3) {
+			c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+		}
+		c = "0x" + c.join("");
+		let r = (c >> 16) & 255;
+		let g = (c >> 8) & 255;
+		let b = c & 255;
 
-    return [r, g, b];
-  } else {
-    throw err;
-  }
+		return [r, g, b];
+	} else {
+		throw err;
+	}
 }
 
 //Calculate luminance
 function luminance(r, g, b) {
-  var a = [r, g, b].map(function (v) {
-    v /= 255;
-    return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
-  });
-  return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+	var a = [r, g, b].map(function (v) {
+		v /= 255;
+		return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+	});
+	return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
 }
 
 //Calculate contrast
 function contrast(rgb1, rgb2) {
-  var lum1 = luminance(rgb1[0], rgb1[1], rgb1[2]);
-  var lum2 = luminance(rgb2[0], rgb2[1], rgb2[2]);
-  var brightest = Math.max(lum1, lum2);
-  var darkest = Math.min(lum1, lum2);
-  return (brightest + 0.05) / (darkest + 0.05);
+	var lum1 = luminance(rgb1[0], rgb1[1], rgb1[2]);
+	var lum2 = luminance(rgb2[0], rgb2[1], rgb2[2]);
+	var brightest = Math.max(lum1, lum2);
+	var darkest = Math.min(lum1, lum2);
+	return (brightest + 0.05) / (darkest + 0.05);
 }
+
+//HTML template
+const HTMLContent = (NewTheme) => {
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<style>
+		body{
+			display: grid;
+			grid-template-columns: repeat(auto-fit,minmax(150px,1fr));
+			background-color:${Config.backgroundColorHTML}
+		}
+		div{
+			height:150px;
+			width:150px;
+		   display: flex;
+		   align-items: center;
+		   justify-content: center;
+
+		}
+		p{
+			background-color: rgba(255,255,255,.5);
+			color:black;
+			display: flex;
+			padding:4px;
+			font-family:sans-serif;
+		}
+
+	</style>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+	</head>
+	<body>
+
+	</body>
+	<script>
+		const colors=${JSON.stringify(NewTheme)}
+		for(token in colors){
+			let node=document.createElement("div");
+			let p=document.createElement("p");
+			node.style.backgroundColor=colors[token].value;
+			p.innerText=token;
+			node.appendChild(p);
+		   document.body.appendChild(node);
+		}
+	</script>
+	</html>`;
+};
